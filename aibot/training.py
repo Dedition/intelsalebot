663e5dfe8c6809e2693f586ae8a24e76bd5087cd
Leo Lad <87998262+Dedition@users.noreply.github.com>
2023-01-24 18:42:33 -0500

    Created import modules

---
+++ aibot/training.py
@@ -0,0 +1,11 @@
+import random
+import numpy as np
+import pickle
+import json
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
