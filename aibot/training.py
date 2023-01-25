8cd52330c37cd3c4e68c158ddfdcb0aa150b1f7a
Leo Lad <87998262+Dedition@users.noreply.github.com>
2023-01-24 20:24:55 -0500

    Created training data

--- aibot/training.py
+++ aibot/training.py
@@ -8,6 +8,7 @@ from nltk.stem import WordNetLemmatizer
 # print(intents)
 
 # nltk.download('punkt')
+# nltk.download('wordnet')
 
 # from tensorflow.keras.models import Sequential
 # from tensorflow.keras.layers import Dense, Activation, Dropout
@@ -30,4 +31,26 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-print(documents)
+words = [lemmatizer.lemmatize(word)
+         for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(words, open('classes.pkl', 'wb'))
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(
+        word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
