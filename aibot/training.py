13f9f1366b1fcfc2de46afefb854e6a49ced7e68
Leo Lad <87998262+Dedition@users.noreply.github.com>
2023-01-24 19:31:49 -0500

    Fixed my intents

--- aibot/training.py
+++ aibot/training.py
@@ -2,10 +2,32 @@ import random
 import numpy as np
 import pickle
 import json
-
 import nltk
 from nltk.stem import WordNetLemmatizer
+# intents = json.loads(open('intents.json').read())
+# print(intents)
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optimizers import SGD
+# nltk.download('punkt')
+
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Activation, Dropout
+# from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer
+
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.append(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+print(documents)
