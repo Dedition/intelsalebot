6d55b7a3b00b5dcee56f2a195ad021d5bc45cdbb
Leo Lad <87998262+Dedition@users.noreply.github.com>
2023-01-25 00:31:30 -0500

    Created clean up function for sentences

--- aibot/chatbot.py
+++ aibot/chatbot.py
@@ -14,3 +14,15 @@ intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('chatbot_model.h5')
+
+
+def clean_up_sentence(sentence):
+    '''
+    Tokenize the sentence and lemmatize each word - create base word,
+    in attempt to represent related words (example: dogs, doggo, doggy
+    => dog) - remove punctuation
+    '''
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word)
+                      for word in sentence_words]
+    return sentence_words
